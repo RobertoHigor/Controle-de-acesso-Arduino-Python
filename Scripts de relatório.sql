b8f065ea901e72f79f996d2f88fcd592377b2cf1
robertohigor <higor15762@live.com>
2019-11-05 11:52:19 -0300

    Alterado script de hora para associar o nome.

--- Scripts de relatório.sql
+++ Scripts de relatório.sql
@@ -8,6 +8,17 @@ INNER JOIN
 )AS a
 ON b.id=a.usuario_id;
 
+--Horas acessadas em ordem (pegar mais cedo e mais tarde)
+SELECT a.first_name, a.last_name, to_char(b.data_acesso, 'HH24:MI') as Hora
+FROM monitoramento_registro as b
+INNER JOIN
+(
+	SELECT *
+	FROM auth_user
+)AS A
+on b.usuario_id = a.id
+ORDER BY Hora DESC;
+
 --Checar hora mais acessada
 SELECT EXTRACT (HOUR FROM data_acesso) as Hora,COUNT(*) AS Quantidade
 FROM monitoramento_registro
@@ -21,11 +32,6 @@ FROM monitoramento_registro
 GROUP BY EXTRACT (HOUR FROM data_acesso)
 ORDER BY Quantidade ASC
 LIMIT 10;
-
---Horas acessadas em ordem (pegar mais cedo e mais tarde)
-SELECT to_char(data_acesso, 'HH24:MI') as Hora
-FROM monitoramento_registro
-ORDER BY Hora ASC;
 
 --Checar dia mais acessado
 SELECT EXTRACT (DAY FROM data_acesso) as Dia,COUNT(*) AS Quantidade
