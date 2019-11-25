20a453a4d26f03987d2312d273b8b01b687ea2b0
robertohigor <higor15762@live.com>
2019-11-25 14:30:49 -0300

    Pequena alteração no script de consulta

--- Scripts de relatório.sql
+++ Scripts de relatório.sql
@@ -9,7 +9,8 @@ INNER JOIN
 ON b.id=a.usuario_id;
 
 --Horas acessadas em ordem (pegar mais cedo e mais tarde)
-SELECT a.first_name, a.last_name, to_char(b.data_acesso, 'HH24:MI') as Hora
+SELECT a.first_name, a.last_name, to_char(b.data_acesso, 'DD/MM HH24:MI'), 
+to_char(b.data_acesso, 'HH24:MI') as Hora
 FROM monitoramento_registro as b
 INNER JOIN
 (
@@ -17,7 +18,7 @@ INNER JOIN
 	FROM auth_user
 )AS A
 on b.usuario_id = a.id
-ORDER BY Hora DESC;
+ORDER BY Hora ASC;
 
 --Checar hora mais acessada
 SELECT EXTRACT (HOUR FROM data_acesso) as Hora,COUNT(*) AS Quantidade
@@ -37,6 +38,12 @@ LIMIT 10;
 SELECT EXTRACT (DAY FROM data_acesso) as Dia,COUNT(*) AS Quantidade
 FROM monitoramento_registro
 GROUP BY EXTRACT (DAY FROM data_acesso)
+ORDER BY Quantidade DESC
+LIMIT 5;
+
+SELECT date(data_acesso) as Dia,COUNT(*) AS Quantidade
+FROM monitoramento_registro
+GROUP BY date(data_acesso)
 ORDER BY Quantidade DESC
 LIMIT 5;
 
